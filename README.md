385d38dbf72f60c2b67e11436d2027bd0a4f6a76
Leland <lelandgutkin@gmail.com>
2024-02-10 15:35:37 -0500

    added index.js to server directory
    For connecting to RDS for PostgreSQL database
    
    Seemse to be an issue where we need SSL certificates for encryption purposes

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Dynamic-Pricing-System
+
+npm init -y in server and client folders to initialize
+type database passwd into server/index.js
+type node index.js to connect to database
+type node server.js to activate server
+
+ctrl + c to end
---
+++ server/index.js
@@ -0,0 +1,18 @@
+const { Client } = require('pg');
+
+const client = new Client({
+    host:"lula-dynamicpricing-testdb.ca3vbbjlumqp.us-east-1.rds.amazonaws.com",
+    port:5432,
+    user:"lulapricingtest",
+    password:"INSERT PASS HERE",
+    database:"lula-dynamicpricing-testdb"
+})
+
+client.connect();
+
+client.query('select * From demo_table', (err, result) => {
+    if(!err){
+        console.log(result.rows);
+    }
+    client.end;
+})
