ea8c6d24116cf2a719c91165d5e5d10224b25abe
SebyMRoy <smr454@drexel.edu>
2024-04-01 22:14:25 -0400

    Adding dashboard
    adding dasboard at the top and added ccs file for dynamic priicing menu

--- client/src/DynamicPricingMenu.js
+++ client/src/DynamicPricingMenu.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 function DynamicPricingMenu() {
   return (
-    <div>
-      <h3>Dynamic Pricing</h3>
-      <p>This is where you see sales history of enrolled products, currently enrolled products, etc.</p>
-      {/* You can add more content specific to the Dynamic Pricing menu */}
+    <div className="dashboard">
+      <h2>Dashboard</h2>
+      <p>Metrics Coming Soon</p>
+      <div className="pricing-menu">
+        <h3>Dynamic Pricing</h3>
+        <p>This is where you see sales history of enrolled products, currently enrolled products, etc.</p>
+        {/* You can add more content specific to the Dynamic Pricing menu */}
+      </div>
     </div>
   );
 }
