d5ffa522fa6abd24098393e20d20e0f9d29eb959
SebyMRoy <smr454@drexel.edu>
2024-04-02 15:37:48 -0400

    Editing dynaimc pricing page
    fixed the dashboard and created popup

--- client/src/DynamicPricingMenu.js
+++ client/src/DynamicPricingMenu.js
@@ -10,20 +10,6 @@ function DynamicPricingMenu() {
     setShowEnrolledProducts(!showEnrolledProducts);
   };
 
-  // Dummy data for enrolled products
-  const enrolledProducts = [
-    { id: 1, name: 'Product A', price: 10 },
-    { id: 2, name: 'Product B', price: 15 },
-    { id: 3, name: 'Product C', price: 5 },
-    { id: 4, name: 'Product D', price: 20 },
-    { id: 5, name: 'Product E', price: 8 },
-    { id: 6, name: 'Product F', price: 2 },
-    { id: 7, name: 'Product G', price: 1 },
-    { id: 8, name: 'Product H', price: 7 },
-    { id: 9, name: 'Product I', price: 12 },
-    { id: 10, name: 'Product J', price: 18 },
-  ];
-
   return (
     <div>
       {/* Dashboard */}
@@ -45,25 +31,6 @@ function DynamicPricingMenu() {
           <div className="popup-content">
             {/* Exit button */}
             <button className="exit-button" onClick={toggleEnrolledProducts}>Exit</button>
-
-            {/* Filter buttons */}
-            <div className="filter-buttons">
-              <button className="calendar-button">Calendar</button>
-              <input type="text" placeholder="Search..." className="search-bar" />
-              <button className="filter-button">Filter by Category</button>
-            </div>
-
-            {/* Enrolled products list */}
-            <div className="enrolled-products-list">
-              <h3>Products</h3>
-              <ul>
-                {enrolledProducts.map(product => (
-                  <li key={product.id}>
-                    {product.name} - ${product.price}
-                  </li>
-                ))}
-              </ul>
-            </div>
           </div>
         </div>
       )}
