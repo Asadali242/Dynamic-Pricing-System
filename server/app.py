d0850cf6371bcdce84b69b5c79c1fa686f934b8f
Leland <lelandgutkin@gmail.com>
2024-04-08 20:57:14 -0400

    minor fix

--- server/app.py
+++ server/app.py
@@ -130,9 +130,9 @@ def seasonal_update():
 def new_minute_update():
     print("A minute has passed")
 
-scheduler.add_job(database.hourly_update, 'cron', hour='*')  
-scheduler.add_job(database.new_minute_update, 'cron', minute='*')
-scheduler.add_job(database.seasonal_update, 'cron', month='3,6,9,12', day='1', hour='0', minute='0')  
+scheduler.add_job(hourly_update, 'cron', hour='*')  
+scheduler.add_job(new_minute_update, 'cron', minute='*')
+scheduler.add_job(seasonal_update, 'cron', month='3,6,9,12', day='1', hour='0', minute='0')  
 
 # Register the shutdown function
 atexit.register(lambda: scheduler.shutdown())
