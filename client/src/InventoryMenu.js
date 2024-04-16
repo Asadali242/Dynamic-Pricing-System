964f80a09be4cc35ef50b230de24d57bf1cb31f9
SebyMRoy <smr454@drexel.edu>
2024-04-16 19:56:40 -0400

    Adjusting item list and buttons
    Adjusted the item list to add a scrolling option and fixxed the buttons and layout

--- client/src/InventoryMenu.js
+++ client/src/InventoryMenu.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import './InventoryMenu.css'; // Import CSS file for styling
 
 function InventoryMenu() {
   const [selectedCategory, setSelectedCategory] = useState(null);
